2999b35b38595ffa4cd6279e512a18ceb4e03f40
Jerett <43426004+dad-of-code@users.noreply.github.com>
2023-06-24 10:06:46 -0600

    Create brainsyncai.sql

---
+++ brainsyncai.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 24, 2023 at 03:49 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `brainsyncai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prompts`
+--
+
+DROP TABLE IF EXISTS `prompts`;
+CREATE TABLE IF NOT EXISTS `prompts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `prompt` text COLLATE utf8mb4_general_ci,
+  `response` text COLLATE utf8mb4_general_ci,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
